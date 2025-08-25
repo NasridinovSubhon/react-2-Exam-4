6ae52f564e7563ddbf626932a5c611d3b3b45142
NasridinovSubhon <subhonnasridinov0@gmail.com>
2025-08-25 12:39:27 +0500

    .

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,11 +1,8 @@
 import axios from "axios"
 
-const navigate = useNavigate()
-
 export const Api = axios.create({
   baseURL: `https://store-api.softclub.tj/`,
 })
-
 
 Api.interceptors.request.use((config) => {
   const token = localStorage.getItem("accessToken");
@@ -19,8 +16,7 @@ Api.interceptors.response.use(
   (res) => res,
   (err) => {
     if (err.response?.status === 401) {
-      navigate("/Login")
+      window.location.href = "/Login"
     }
-    return Promise.reject(err)
   }
 )
